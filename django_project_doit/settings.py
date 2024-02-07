6420edab3b192f41afb39c32483ba65dd529c36b
EC2 Default User <ec2-user@ip-172-26-24-148.ap-northeast-2.compute.internal>
2024-02-07 16:14:09 +0900

    수정

--- django_project_doit/settings.py
+++ django_project_doit/settings.py
@@ -46,6 +46,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = ["django.netscrap.co.kr", "http://django.netscrap.co.kr", "https://django.netscrap.co.kr"]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -64,7 +67,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    #"django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
